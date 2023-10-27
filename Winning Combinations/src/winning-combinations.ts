5947c5d5b6c27d0e86e07ca078e3f54ec2661a26
David Mees Knijnik <kdavidk98@gmail.com>
2023-10-27 16:35:47 -0300

    Comment Capitalization

--- Winning Combinations/src/winning-combinations.ts
+++ Winning Combinations/src/winning-combinations.ts
@@ -7,10 +7,10 @@ function call(lines: number[]): WinningCombinationsResult {
   let repeatedSymbol:number = 0;
   let symbolCombinationLocation:number[] = [];
   let wilds:number[] = [];
-  //iterates over the symbols in the line
+  //Iterates over the symbols in the line
   for(let symbolIndex=0; symbolIndex<lines.length; symbolIndex++){
     if((lines[symbolIndex] === repeatedSymbol) || (lines[symbolIndex] === 0)){
-      //adds symbol to combination if it is repeated or is wild
+      //Adds symbol to combination if it is repeated or is wild
       symbolCombinationLocation.push(symbolIndex);
     }
     else{
